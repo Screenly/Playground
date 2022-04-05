2f798fa9437c37d0118a008ec1356e2ffed34b0c
Devan Thakur <devanthakur89@gmail.com>
2022-04-05 13:46:39 +0530

    ESlint - Remove semicolons

--- javascript-injectors/shiphero-signin-via-credentials.js
+++ javascript-injectors/shiphero-signin-via-credentials.js
@@ -1,32 +1,32 @@
 (function () {
-  const email = '<YOUR_EMAIL>';
-  const password = '<YOUR_PASSWORD>';
+  const email = '<YOUR_EMAIL>'
+  const password = '<YOUR_PASSWORD>'
 
   function changeValue (input, value) {
     const nativeInputSetter = Object.getOwnPropertyDescriptor(
       window.HTMLInputElement.prototype,
       'value'
-    ).set;
+    ).set
 
-    nativeInputSetter.call(input, value);
+    nativeInputSetter.call(input, value)
 
     const inputEvent = new Event('input', { bubbles: true });
-    input.dispatchEvent(inputEvent);
+    input.dispatchEvent(inputEvent)
   }
 
   function login () {
     try {
-      const emailEl = document.querySelector('input[name="email"]');
-      const passwordEl = document.querySelector('input[name="password"]');
+      const emailEl = document.querySelector('input[name="email"]')
+      const passwordEl = document.querySelector('input[name="password"]')
 
-      changeValue(emailEl, email);
-      changeValue(passwordEl, password);
+      changeValue(emailEl, email)
+      changeValue(passwordEl, password)
 
-      document.querySelector('button[name="submit"]').click();
+      document.querySelector('button[name="submit"]').click()
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
 
-  setTimeout(login, 5000);
+  setTimeout(login, 5000)
 })();
