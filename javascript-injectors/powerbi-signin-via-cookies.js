d54e51820e6a807f745e6aa34f19a733ca47913f
Rusko124 <psafronov@screenly.io>
2022-04-14 17:39:26 +0300

    Fix: javascript-injectors lint

--- javascript-injectors/powerbi-signin-via-cookies.js
+++ javascript-injectors/powerbi-signin-via-cookies.js
@@ -1,30 +1,30 @@
-function isCookieExist(key) {
-    return document.cookie.indexOf(key + "=") !== -1
+function isCookieExist (key) {
+  return document.cookie.indexOf(key + '=') !== -1
 }
 
-function setCookie(key, value) {
-    if (isCookieExist(key)) {
-        return false
-    }
-    document.cookie = key + '=' + value + '; secure; path=/;';
-    return true
+function setCookie (key, value) {
+  if (isCookieExist(key)) {
+    return false
+  }
+  document.cookie = key + '=' + value + '; secure; path=/;'
+  return true
 }
 
-const url = "https://app.powerbi.com/home"
+const url = 'https://app.powerbi.com/home'
 const cookies = {
-    ".AspNet.CookiesC1": "<COOKIE_VALUE>",
-    ".AspNet.CookiesC2": "<COOKIE_VALUE>",
-    ".AspNet.Cookies": "chunks:2",
+  '.AspNet.CookiesC1': '<COOKIE_VALUE>',
+  '.AspNet.CookiesC2': '<COOKIE_VALUE>',
+  '.AspNet.Cookies': 'chunks:2',
 }
 
-let isCookiesApplied = false;
+let isCookiesApplied = false
 
 for (const cookieKey in cookies) {
-    if (setCookie(cookieKey, cookies[cookieKey])) {
-        isCookiesApplied = true
-    }
+  if (setCookie(cookieKey, cookies[cookieKey])) {
+    isCookiesApplied = true
+  }
 }
 
 if (isCookiesApplied) {
-    window.location.replace(url)
+  window.location.replace(url)
 }
--- javascript-injectors/powerbi-signin-via-credentials.js
+++ javascript-injectors/powerbi-signin-via-credentials.js
@@ -1,44 +1,44 @@
 (function () {
-    if (typeof url !== 'undefined') {
-        return
-    }
+  if (typeof url !== 'undefined') {
+    return
+  }
 
-    var url = "https://app.powerbi.com/home";
-    const username = "<YOUR_USERNAME>";
-    const password = "<YOUR_PASSWORD>";
+  var url = 'https://app.powerbi.com/home'  // eslint-disable-line
+  const username = '<YOUR_USERNAME>'
+  const password = '<YOUR_PASSWORD>'
 
-    let isSingleSignOnPassed = false;
-    let isOauthPassed = false;
+  let isSingleSignOnPassed = false
+  let isOauthPassed = false
 
-    function callback() {
-        try {
-            if (window.location.pathname === '/singleSignOn' && !isSingleSignOnPassed) {
-                document.querySelector("#email").value = username;
-                submitEmail();
-                isSingleSignOnPassed = true;
-            }
+  function callback () {
+    try {
+      if (window.location.pathname === '/singleSignOn' && !isSingleSignOnPassed) {
+        document.querySelector('#email').value = username
+        submitEmail()  // eslint-disable-line
+        isSingleSignOnPassed = true
+      }
 
-            if (window.location.pathname === '/common/oauth2/authorize' && !isOauthPassed) {
-                if (document.body.innerHTML.search("Pick an account") !== -1) {
-                    const pickAccountEl = document.querySelector('div[data-test-id="' + username + '"]');
-                    pickAccountEl.click();
-                }
-
-                const passwordInput = document.querySelector('input[name="passwd"]');
-                passwordInput.value = password;
-                passwordInput.dispatchEvent(new Event('change'));
-                document.querySelector('input[type="submit"]').click();
-                isOauthPassed = true;
-            }
-
-            if (window.location.pathname === '/common/login') {
-                window.location.replace(url)
-            }
-        } catch (error) {
-            console.error(error)
+      if (window.location.pathname === '/common/oauth2/authorize' && !isOauthPassed) {
+        if (document.body.innerHTML.search('Pick an account') !== -1) {
+          const pickAccountEl = document.querySelector('div[data-test-id="' + username + '"]')
+          pickAccountEl.click()
         }
-    }
 
-    callback();
-    setInterval(callback, 1000);
-})();
+        const passwordInput = document.querySelector('input[name="passwd"]')
+        passwordInput.value = password
+        passwordInput.dispatchEvent(new Event('change'))
+        document.querySelector('input[type="submit"]').click()
+        isOauthPassed = true
+      }
+
+      if (window.location.pathname === '/common/login') {
+        window.location.replace(url)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  callback()
+  setInterval(callback, 1000)
+})()
