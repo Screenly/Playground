75f210eaa3a9f7bdfa18b2c16c3fa0010b8fad32
Devan Thakur <devanthakur89@gmail.com>
2022-05-22 10:28:37 +0530

    Fix lint issues

--- asset-metadata/src/main.py
+++ asset-metadata/src/main.py
@@ -11,17 +11,21 @@ Example headers
     "X-Screenly-tags": "srly-jmar75ko6xp651j,custom-label"
 }"""
 
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template
 
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config.from_pyfile("config.py")
 
 
 @app.route("/")
 def render_metadata_headers():
-    return render_template('metadata_headers.html', headers=request.headers, apiKey=app.config['GMAPS_API_KEY'])
+    return render_template(
+        "metadata_headers.html",
+        headers=request.headers,
+        apiKey=app.config["GMAPS_API_KEY"],
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
