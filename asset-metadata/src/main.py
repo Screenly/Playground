87638fb0fef07f4842dff68cf2d82224be7a4f0f
Devan Thakur <devanthakur89@gmail.com>
2022-05-27 17:45:50 +0530

    Add Google Maps API key

--- asset-metadata/src/main.py
+++ asset-metadata/src/main.py
@@ -11,6 +11,7 @@ Example headers
     "X-Screenly-tags": "srly-jmar75ko6xp651j,custom-label"
 }"""
 
+from os import environ
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -22,7 +23,7 @@ def render_metadata_headers():
     return render_template(
         "metadata_headers.html",
         headers=request.headers,
-        apiKey=app.config["GMAPS_API_KEY"],
+        apiKey=environ.get('GOOGLE_MAPS_API_KEY'),
     )
 
 
