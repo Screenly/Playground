93048e81881b7d109ebf2fd4b500ab7e56887468
Devan Thakur <devanthakur89@gmail.com>
2022-12-01 15:22:10 +0530

    Adds instagram theme

---
+++ bootstrap/instagram/src/assets/scripts/script.js
@@ -0,0 +1,46 @@
+(function () {
+  function displayPosts (posts) {
+    const container = document.querySelector('.cards')
+    const placeholder = document.querySelector('.dummy-card')
+    const fragment = document.createDocumentFragment()
+
+    posts.forEach(({ username, caption, media_url }) => {
+      const node = placeholder.cloneNode(true)
+      const imgNode = node.querySelector('.post-image')
+      const captionNode = node.querySelector('.caption')
+
+      imgNode.setAttribute('src', media_url)
+      captionNode.innerHTML = `<span class="user-handle">@${username}</span> ${caption || ''}`
+      node.classList.remove('dummy-card')
+      fragment.appendChild(node)
+    })
+
+    container.appendChild(fragment)
+  }
+
+  function fetchPosts () {
+    var token = "IGQVJWSDlHSG9xakxGT3p3Y3BFOTctVEc3VWtFNnhPV3VwMlhNcGJMV2cxTFhHZAnB3MDZAEZAHNpZAW12WUhvM2RrSzhYdWtyTmQ0RjJ4U0luMWdBMUJUWHREY1BZAVVNYVXI3TFlaVlNPNWZAMQTJhNGZAyYwZDZD"
+    const url = `https://graph.instagram.com/me/media?fields=username,caption,media_url,media_type&access_token=${token}`
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        const { data: posts } = data;
+        const latestTwoPosts = [];
+        for (index = 0; index < posts.length; index++) {
+          if (posts[index].media_type === 'IMAGE') {
+            latestTwoPosts.push(posts[index])
+          }
+
+          if (latestTwoPosts.length === 2) {
+            break;
+          }
+        }
+        displayPosts(latestTwoPosts)
+      })
+      .catch((e) => {
+        console.log(e)
+      });
+  }
+
+  fetchPosts()
+}) ()
