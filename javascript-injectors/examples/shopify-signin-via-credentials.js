7fb7ac0b4597c8ca3540051eeee9edaef66657d4
iamlawrenceonoja <iamlawrenceonoja@gmail.com>
2023-03-28 18:32:28 +0100

    Edited javascript injectors documentation to include shopify example and Fixed lint errors

--- javascript-injectors/examples/shopify-signin-via-credentials.js
+++ javascript-injectors/examples/shopify-signin-via-credentials.js
@@ -6,32 +6,32 @@
   const email = '<EMAIL>'
   const password = '<PASSWORD>'
 
-function setValue (selector, value) {
-  const element = document.getElementById(selector)
-  element.value = value
-  element.dispatchEvent(new Event('change'))
-}
-
-function submitForm () {
-  document.querySelector('button[name="commit"]').click()
-}
-
-function login () {
-  try {
-    if (window.location.pathname === '/lookup') {
-      setValue('account_email', email)
-      submitForm()
-    }
-
-    if (window.location.pathname === '/login') {
-      setValue('account_password', password)
-      submitForm()
-    }
-  } catch (error) {
-    console.error(error)
+  function setValue (selector, value) {
+    const element = document.getElementById(selector)
+    element.value = value
+    element.dispatchEvent(new Event('change'))
   }
-}
 
-login()
-setInterval(login, 1000)
+  function submitForm () {
+    document.querySelector('button[name="commit"]').click()
+  }
+
+  function login () {
+    try {
+      if (window.location.pathname === '/lookup') {
+        setValue('account_email', email)
+        submitForm()
+      }
+
+      if (window.location.pathname === '/login') {
+        setValue('account_password', password)
+        submitForm()
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  login()
+  setInterval(login, 1000)
 })()
