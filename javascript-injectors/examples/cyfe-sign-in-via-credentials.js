bf60828d2f8eda0f3c91ea2a41caa675969e31e0
iamlawrenceonoja <iamlawrenceonoja@gmail.com>
2023-05-02 20:12:30 +0100

    Added javascript injector snippet for logging into Cyfe

---
+++ javascript-injectors/examples/cyfe-sign-in-via-credentials.js
@@ -0,0 +1,32 @@
+(function () {
+  const password = '<PASSWORD>'
+
+  function elExists (selector) {
+    const el = document.querySelector(selector)
+    return el !== undefined && el.getAttribute('aria-hidden') !== 'true'
+  }
+
+  function setValue (selector, value) {
+    const element = document.querySelector(selector)
+    element.value = value
+    element.dispatchEvent(new Event('change'))
+  }
+
+  function submitForm () {
+    document.querySelector('input[type="submit"]').click()
+  }
+
+  function login () {
+    try {
+      setValue('input[name="password"]', password)
+      submitForm()
+    } catch (error) {
+      console.warn(error)
+      setTimeout(login, 3000)
+    }
+  }
+
+  if (elExists('input[name=password]')) {
+    login()
+  }
+})()
