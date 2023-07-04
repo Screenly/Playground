00f460efd7bdcd35347f8d437fa73ff11369e168
iamlawrenceonoja <iamlawrenceonoja@gmail.com>
2023-07-04 13:37:52 +0100

    Fix lint errors

--- javascript-injectors/examples/tableau-cookies-consent-closing.js
+++ javascript-injectors/examples/tableau-cookies-consent-closing.js
@@ -1,6 +1,6 @@
-setInterval(function() {
+setInterval(function () {
   const acceptCookie = document.querySelector('button[id="onetrust-accept-btn-handler"]')
   if (acceptCookie) {
     acceptCookie.click()
-  }}, 2000
+  } }, 2000
 )
