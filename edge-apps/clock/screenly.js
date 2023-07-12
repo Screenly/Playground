15e7b9d4998865712ee888bea2c7fbe5770cc999
nicomiguelino <nicomiguelino2014@gmail.com>
2023-07-12 00:42:03 -0700

    Modify `index.html` so that it makes use of metadata and settings the Edge App way.

--- edge-apps/clock/screenly.js
+++ edge-apps/clock/screenly.js
@@ -1,4 +1,23 @@
 // eslint-disable-next-line no-unused-vars
-function screenlyMetadataEndpoint () {
-  return 'http://localhost:3004/metadata'
-}
+// @TODO: This file mocks the actual `screenly.js`. Remove this afterwards.
+const screenly = {
+  "metadata": {
+      "coordinates": [
+          "59.93433",
+          "30.31453"
+      ],
+      "hardware": "unknown",
+      "hostname": "srly-hbnon7psavslk8v",
+      "location": "Saint Petersburg, Russia",
+      "screen_name": "sergey pi4 dev 1",
+      "screenly_version": "unknown",
+      "tags": [
+          "All Screens",
+          "sergey pi4 dev 1"
+      ]
+  },
+  "settings": {
+      "ga_api_key": "mcd0n4ld5",
+      "sentry_id": "p4nd43xpr355",
+  }
+};
--- edge-apps/clock/static/js/main.js
+++ edge-apps/clock/static/js/main.js
@@ -28,3 +28,5 @@ function initApp () {
 
   initDateTime()
 }
+
+initApp()
