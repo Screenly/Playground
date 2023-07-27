404a939bf7247072f925f4762eec12a26bc8a077
nicomiguelino <nicomiguelino2014@gmail.com>
2023-07-27 00:31:35 -0700

    Overhaul layout so that the RSS Reader Edge App will support multiple screen resolutions.

--- edge-apps/rss-reader/static/js/main.js
+++ edge-apps/rss-reader/static/js/main.js
@@ -1,11 +1,22 @@
+// @TODO: Process feed content to handle images and text accordingly.
+//   Add conditional statements to handle different types of content.
+const processFeedContent = ({ content, contentSnippet }) => {
+  const parser = new DOMParser()
+  const doc = parser.parseFromString(content, 'text/html')
+
+  return contentSnippet
+}
+
 const initApp = () => {
-  const feedsContainer = document.querySelector('#feeds-container')
+  const feedsContainer = document
+    .querySelector('#feeds-container')
+    .querySelector('.row')
   const { rss_url, rss_title, limit } = screenly.settings
   const parser = new RSSParser()
 
   const titleHeader = document.querySelector('#rss-title')
-  document.title = rss_title
   titleHeader.innerHTML = rss_title
+  document.title = rss_title
 
   parser.parseURL(rss_url, (err, feed) => {
     if (err) {
@@ -18,17 +29,16 @@ const initApp = () => {
       const title = entry.title
       const date = moment(new Date(entry.pubDate))
         .format('MMMM DD, YYYY, h:mm A')
-      // @TODO: Handle content dynamically. Some have images, some have just text,
-      // some have both.
-      const description = entry.content
 
       const feedTemplate = document.querySelector('#feed-template')
       const feedContainer = feedTemplate.content.cloneNode(true)
 
       feedContainer.querySelector('.feed-title').innerHTML = title
       feedContainer.querySelector('.feed-date').innerHTML = date
-      feedDescription = feedContainer.querySelector('.feed-description')
-      feedDescription.innerHTML = description
+      feedDescription = feedContainer
+        .querySelector('.feed-description')
+        .querySelector('p')
+      feedDescription.innerHTML = processFeedContent(entry)
 
       feedsContainer.appendChild(feedContainer)
     })
