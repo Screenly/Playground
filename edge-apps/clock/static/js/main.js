b45a58fbe8bca5c3242920730d5a3e6826431b03
nicomiguelino <nicomiguelino2014@gmail.com>
2023-08-07 11:24:06 -0700

    Use an alternative library for looking up TZ using lat/long.

--- edge-apps/clock/static/js/main.js
+++ edge-apps/clock/static/js/main.js
@@ -5,19 +5,6 @@ function initApp () {
   let clockTimer
   const latitude = screenly.metadata.coordinates[0];
   const longitude = screenly.metadata.coordinates[1];
-  const googleMapsTimezoneApiUrl = 'https://maps.googleapis.com/maps/api/timezone/json'
-  const apiKey = screenly.settings.gm_api_key;
-
-  const getTimezoneFromCoordinates = async (latitude, longitude) => {
-    const queryParams = [
-      `location=${latitude},${longitude}`,
-      `timestamp=${Date.now() / 1000}`,
-      `key=${apiKey}`
-    ].join('&')
-    const response = await fetch(`${googleMapsTimezoneApiUrl}?${queryParams}`)
-    const data = await response.json()
-    return data.timeZoneId
-  }
 
   const locale = navigator?.languages?.length
     ? navigator.languages[0]
@@ -36,14 +23,13 @@ function initApp () {
   const initDateTime = () => {
     clearTimeout(clockTimer)
 
-    getTimezoneFromCoordinates(latitude, longitude).then(timezoneFromCoordinates => {
-      const momentObject = (screenly.settings?.timezone != '')
-        ? moment().tz(screenly.settings.timezone)
-        : moment().tz(timezoneFromCoordinates)
+    const timezone = (screenly.settings?.override_timezone != '')
+      ? screenly.settings.override_timezone
+      : tzlookup(latitude, longitude)
+    const momentObject = moment().tz(timezone)
 
-      document.querySelector('#date').innerText = momentObject.format('DD MMMM, YYYY')
-      document.querySelector('#time').innerHTML = formatTime(momentObject)
-    })
+    document.querySelector('#date').innerText = momentObject.format('DD MMMM, YYYY')
+    document.querySelector('#time').innerHTML = formatTime(momentObject)
 
     clockTimer = setTimeout(initDateTime, 10000)
   }
