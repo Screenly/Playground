6bb492b1a0245c333c3465f348da550bda399ac6
Viktor Petersson <self@vpetersson.com>
2023-08-23 09:53:50 +0100

    Tag Manager does not need to be a secret

---
+++ edge-apps/clock/src/static/js/load-screenly-data.js
@@ -0,0 +1,31 @@
+/* global loadMetadata */
+(function () {
+  //function loadSettings () {
+  //  const script = document.createElement('script')
+  //  script.src = './screenly.js?version=1'
+  //  script.async = true
+  //  script.onload = loadSettings
+  //  script.onerror = function () {
+  //    console.warn('screenly.js is not available.')
+  //    loadMetadata()
+  //  }
+  //  document.head.appendChild(script)
+  //}
+
+  //const screenly_settings = loadSettings();
+  const screenly_settings = {
+    "coordinates": [
+      "51.5085300",
+      "-0.1257400"
+    ],
+    "hardware": "Screenly Player Max",
+    "hostname": "srly-[redacted]",
+    "location": "London, UK",
+    "screen_name": "Lobby Screen",
+    "screenly_version": "Screenly 2 Player 3.4.7-x86-1595 (amd64)",
+    "tags": [
+      "All Screens",
+      "Lobby"
+    ]
+  }
+})()
