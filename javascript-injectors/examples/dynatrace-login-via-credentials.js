543a6b9593d7159499bec8ae4fbc919afe3c3e44
iamlawrenceonoja <iamlawrenceonoja@gmail.com>
2023-09-12 10:07:59 +0100

    Fixed lint errors

--- javascript-injectors/examples/dynatrace-login-via-credentials.js
+++ javascript-injectors/examples/dynatrace-login-via-credentials.js
@@ -1,31 +1,31 @@
 function dynatraceLogin () {
-  const email = '<USERNAME>'
-  const password = '<PASSWORD>'
+  const email = 'oabah@screenly.io'
+  const password = 'Abcdefghijkl$'
 
   const authLocation = '/action/signin'
 
   function setValue (selector, value) {
     const element = document.querySelector(selector)
-    if (element.value == "") {
+    if (element.value === '') {
       element.value = value
-      element.dispatchEvent(new Event('change')) 
-    } 
+      element.dispatchEvent(new Event('change'))
+    }
   }
 
   function submitEmail () {
-    emailBtn = document.querySelector('button[data-id="email_submit"]')
+    const emailBtn = document.querySelector('button[data-id="email_submit"]')
     emailBtn.click()
   }
 
   function submitPassword () {
-    passwordBtn = document.querySelector('button[data-id="sign_in"]')
+    const passwordBtn = document.querySelector('button[data-id="sign_in"]')
     passwordBtn.click()
   }
 
   function inputEmail () {
     try {
-        setValue('input[name="email"]', email)
-        submitEmail()
+      setValue('input[name="email"]', email)
+      submitEmail()
     } catch (error) {
       console.warn(error)
     }
@@ -33,8 +33,8 @@ function dynatraceLogin () {
 
   function login () {
     try {
-        setValue('input[type="password"]', password)
-        submitPassword()
+      setValue('input[type="password"]', password)
+      submitPassword()
     } catch (error) {
       console.warn(error)
     }
