094d954d9b32f0038d80a639fa2fd8067e39435a
abok istifanus madaki <abokistifanusmadaki@gmail.com>
2023-10-18 08:43:47 +0100

    Fixed lint errors

---
+++ edge-apps/instagram/README.md
@@ -0,0 +1,14 @@
+# Instagram Edge App
+
+An edge app that uses a Meta token to fetch the latest two posts from an Instagram account
+
+```bash
+$ cd edge-apps/instagram
+$ screenly edge-app create \
+    --name Instagram \
+    --in-place
+$ screenly edge-app upload
+[...]
+$ screenly edge-app secret set instagram_api_token=MY_META_TOKEN
+$ screenly edge-app version promote revision=X
+```
--- edge-apps/instagram/static/js/main.js
+++ edge-apps/instagram/static/js/main.js
@@ -19,7 +19,7 @@
   }
 
   function fetchPosts () {
-    const token = 'INSTAGRAM_API_TOKEN'
+    const token = screenly.settings.instagram_api_token
     const url = `https://graph.instagram.com/me/media?fields=username,caption,media_url,media_type&access_token=${token}`
     fetch(url)
       .then(response => response.json())
@@ -27,7 +27,7 @@
         const { data: posts } = data
         const latestTwoPosts = []
         for (let index = 0; index < posts.length; index++) {
-          if (posts[index].media_type === 'IMAGE') {
+          if (posts[index].media_type === 'CAROUSEL_ALBUM' || posts[index].media_type === 'IMAGE') {
             latestTwoPosts.push(posts[index])
           }
 
