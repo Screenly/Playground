f18c56b2e227204425c70e2936768ad5f2249e2a
nicomiguelino <nicomiguelino2014@gmail.com>
2023-10-18 12:34:29 -0700

    style: Remove extra semicolons on JS code.

--- edge-apps/clock/static/js/main.js
+++ edge-apps/clock/static/js/main.js
@@ -1,73 +1,73 @@
 /* global clm, moment, OfflineGeocodeCity */
 // eslint-disable-next-line no-unused-vars
 
-const { getNearestCity } = OfflineGeocodeCity;
-const allTimezones = moment.tz.names();
+const { getNearestCity } = OfflineGeocodeCity
+const allTimezones = moment.tz.names()
 
 async function initApp () {
-  let clockTimer;
-  const { metadata, settings } = screenly;
-  const latitude = metadata.coordinates[0];
-  const longitude = metadata.coordinates[1];
+  let clockTimer
+  const { metadata, settings } = screenly
+  const latitude = metadata.coordinates[0]
+  const longitude = metadata.coordinates[1]
 
   const defaultLocale = navigator?.languages?.length
     ? navigator.languages[0]
-    : navigator.language;
+    : navigator.language
 
   const getLocale = () => {
-    overrideLocale = settings?.override_locale;
+    overrideLocale = settings?.override_locale
 
     if (overrideLocale) {
       if (moment.locales().includes(overrideLocale)) {
-        return overrideLocale;
+        return overrideLocale
       } else {
-        console.warn(`Invalid locale: ${overrideLocale}. Using defaults.`);
+        console.warn(`Invalid locale: ${overrideLocale}. Using defaults.`)
       }
     }
 
-    const data = getNearestCity(latitude, longitude);
-    const countryCode = data.countryIso2.toUpperCase();
+    const data = getNearestCity(latitude, longitude)
+    const countryCode = data.countryIso2.toUpperCase()
 
-    return clm.getLocaleByAlpha2(countryCode) || defaultLocale;
-  };
+    return clm.getLocaleByAlpha2(countryCode) || defaultLocale
+  }
 
   const formatTime = async (momentObject) => {
-    const locale = getLocale();
-    moment.locale(locale);
-    const time = momentObject.format('LT');
-    const timeFragments = time.split(' ');
+    const locale = getLocale()
+    moment.locale(locale)
+    const time = momentObject.format('LT')
+    const timeFragments = time.split(' ')
 
     return Array.isArray(timeFragments) && timeFragments.length === 2
       ? `${timeFragments[0]}<span>${timeFragments[1]}</span>`
-      : time;
+      : time
   }
 
   const getTimezone = () => {
-    const overrideTimezone = settings?.override_timezone;
+    const overrideTimezone = settings?.override_timezone
     if (overrideTimezone) {
       if (allTimezones.includes(overrideTimezone)) {
-        return overrideTimezone;
+        return overrideTimezone
       } else {
-        console.warn(`Invalid timezone: ${overrideTimezone}. Using defaults.`);
+        console.warn(`Invalid timezone: ${overrideTimezone}. Using defaults.`)
       }
     }
 
-    return tzlookup(latitude, longitude);
-  };
-
-  const initDateTime = async () => {
-    clearTimeout(clockTimer);
-
-    const timezone = getTimezone();
-    const momentObject = moment().tz(timezone);
-
-    document.querySelector('#date').innerText = momentObject.format('LL');
-    document.querySelector('#time').innerHTML = await formatTime(momentObject);
-
-    clockTimer = setTimeout(initDateTime, 10000);
+    return tzlookup(latitude, longitude)
   }
 
-  await initDateTime();
+  const initDateTime = async () => {
+    clearTimeout(clockTimer)
+
+    const timezone = getTimezone()
+    const momentObject = moment().tz(timezone)
+
+    document.querySelector('#date').innerText = momentObject.format('LL')
+    document.querySelector('#time').innerHTML = await formatTime(momentObject)
+
+    clockTimer = setTimeout(initDateTime, 10000)
+  }
+
+  await initDateTime()
 }
 
 initApp()
