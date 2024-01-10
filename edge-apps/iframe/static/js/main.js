fdfd9f39589a9df4ec474b21d4931dba0f25bda3
Salman Faris <farissalmannbr@gmail.com>
2024-01-10 23:13:24 +0530

    fix js lint

--- edge-apps/iframe/static/js/main.js
+++ edge-apps/iframe/static/js/main.js
@@ -1,19 +1,20 @@
-function extractUrlFromIframeSetting(settingValue) {
-    if (settingValue.includes('<iframe')) {
-        const match = settingValue.match(/<iframe.*?src=["'](.*?)["']/);
-        if (match && match[1]) {
-            return match[1];
-        }
-    } else {
-        return settingValue;
-    }
-    return null;
-}
-document.addEventListener("DOMContentLoaded", function () {
-    var iframeSettingValue = screenly.settings.iframe || "iframe URL not set";
-    var iframeUrl = extractUrlFromIframeSetting(iframeSettingValue);
-    var iframeElement = document.getElementById("iframe");
-    iframeElement.src = iframeUrl;
-    iframeElement.innerText = iframeUrl;
-});
+/* global  screenly */
 
+function extractUrlFromIframeSetting (settingValue) {
+ if (settingValue.includes('<iframe')) {
+    const match = settingValue.match(/<iframe.*?src=["'](.*?)["']/)
+    if (match && match[1]) {
+      return match[1]
+    }
+  } else {
+    return settingValue
+  }
+  return null
+}
+document.addEventListener('DOMContentLoaded', function () {
+  const iframeSettingValue = screenly.settings.iframe || 'iframe URL not set'
+  const iframeUrl = extractUrlFromIframeSetting(iframeSettingValue)
+  const iframeElement = document.getElementById('iframe')
+  iframeElement.src = iframeUrl
+  iframeElement.innerText = iframeUrl
+})
