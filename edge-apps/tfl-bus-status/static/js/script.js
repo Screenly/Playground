08aa8940dd4cfcc9d54f3f6ea10ade8c924750ab
Salman Faris <farissalmannbr@gmail.com>
2024-05-15 14:44:57 +0800

    fix lint errors by fixing camel case

--- edge-apps/tfl-bus-status/static/js/script.js
+++ edge-apps/tfl-bus-status/static/js/script.js
@@ -36,17 +36,13 @@ async function fetchBusData () {
     // Some station does not have towards station, In that situation - We are hiding the Towards text.
     if (stationTowards === 'null') {
       busStopName.innerHTML = `${stationName}`
-    }
-    else {
-      busStopName.innerHTML = `${stationName} - Towards: ${stationTowards}`
-    }
+    } else {
+      busStopName.innerHTML = `${stationName} - Towards: ${stationTowards}`}
 
     if (stationPlatform === 'null') {
       busPlatform.innerHTML = `Bus Arrivals`
-    }
-    else {
-      busPlatform.innerHTML = `Bus Arrivals: Platform - ${stationPlatform}`
-    }
+    } else {
+      busPlatform.innerHTML = `Bus Arrivals: Platform - ${stationPlatform}` }
 
     // Fetch Bus Line ID and If bus is not available, assigned to Zero.
     const bus1LineID = nextBuses[0] && nextBuses[0].lineId ? nextBuses[0].lineId : 0
@@ -62,46 +58,46 @@ async function fetchBusData () {
     // Apply BUS Line status text and CSS as per the line ID.
     const routeStatus1 = document.getElementById('route-status-1')
     // If bus route status is not found, assign 22 as error.
-    const bus1_route_status = (lineData[bus1LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
+    const bus1RouteStatus = (lineData[bus1LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
 
-    routeStatus1.innerHTML = getStatusInfo(bus1_route_status).message
-    routeStatus1.className = getStatusInfo(bus1_route_status).className
+    routeStatus1.innerHTML = getStatusInfo(bus1RouteStatus).message
+    routeStatus1.className = getStatusInfo(bus1RouteStatus).className
 
     // check if the API have bus information, if not, assign zero to hide it.
-    const bus1_Line = nextBuses.length > 0 && nextBuses[0].lineName ? nextBuses[0].lineName : 0
-    const bus1_Destination = nextBuses.length > 0 && nextBuses[0].destinationName ? nextBuses[0].destinationName : 0
-    const bus1_time = nextBuses.length > 0 && nextBuses[0].timeToStation ? Math.floor(nextBuses[0].timeToStation / 60) : 0
+    const bus1Line = nextBuses.length > 0 && nextBuses[0].lineName ? nextBuses[0].lineName : 0
+    const bus1Destination = nextBuses.length > 0 && nextBuses[0].destinationName ? nextBuses[0].destinationName : 0
+    const bus1Time = nextBuses.length > 0 && nextBuses[0].timeToStation ? Math.floor(nextBuses[0].timeToStation / 60) : 0
 
-    if (bus1_Line === 0 && bus1_Destination === 0 && bus1_time === 0) {
+    if (bus1Line === 0 && bus1Destination === 0 && bus1Time === 0) {
       document.querySelector('.bus-item-1').classList.add('hidden')
     }
 
-    document.querySelector('.route-1').innerHTML = `Route ${bus1_Line}&nbsp;`
-    document.querySelector('.destination-1').innerHTML = `(${bus1_Destination})`
-    document.querySelector('.time-1').innerHTML = `${bus1_time} MIN`
+    document.querySelector('.route-1').innerHTML = `Route ${bus1Line}&nbsp;`
+    document.querySelector('.destination-1').innerHTML = `(${bus1Destination})`
+    document.querySelector('.time-1').innerHTML = `${bus1Time} MIN`
 
     // 2nd Bus Details
 
     // Apply BUS Line status text and CSS as per the line ID.
     const routeStatus2 = document.getElementById('route-status-2')
     // If bus route status is not found, assign 22 as error.
-    const bus2_route_status = (lineData[bus2LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
+    const bus2RouteStatus = (lineData[bus2LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
 
-    routeStatus2.innerHTML = getStatusInfo(bus2_route_status).message
-    routeStatus2.className = getStatusInfo(bus2_route_status).className
+    routeStatus2.innerHTML = getStatusInfo(bus2RouteStatus).message
+    routeStatus2.className = getStatusInfo(bus2RouteStatus).className
 
     // check if the API have bus information, if not, assign zero to hide it.
-    const bus2_Line = nextBuses.length > 1 && nextBuses[1].lineName ? nextBuses[1].lineName : 0
-    const bus2_Destination = nextBuses.length > 1 && nextBuses[1].destinationName ? nextBuses[1].destinationName : 0
-    const bus2_time = nextBuses.length > 1 && nextBuses[1].timeToStation ? Math.floor(nextBuses[1].timeToStation / 60) : 0
+    const bus2Line = nextBuses.length > 1 && nextBuses[1].lineName ? nextBuses[1].lineName : 0
+    const bus2Destination = nextBuses.length > 1 && nextBuses[1].destinationName ? nextBuses[1].destinationName : 0
+    const bus2Time = nextBuses.length > 1 && nextBuses[1].timeToStation ? Math.floor(nextBuses[1].timeToStation / 60) : 0
 
-    if (bus2_Line === 0 && bus2_Destination === 0 && bus2_time === 0) {
+    if (bus2Line === 0 && bus2Destination === 0 && bus2Time === 0) {
       document.querySelector('.bus-item-2').classList.add('hidden')
     }
 
-    document.querySelector('.route-2').innerHTML = `Route ${bus2_Line}&nbsp;`
-    document.querySelector('.destination-2').innerHTML = `(${bus2_Destination})`
-    document.querySelector('.time-2').innerHTML = `${bus2_time} MIN`
+    document.querySelector('.route-2').innerHTML = `Route ${bus2Line}&nbsp;`
+    document.querySelector('.destination-2').innerHTML = `(${bus2Destination})`
+    document.querySelector('.time-2').innerHTML = `${bus2time} MIN`
 
 
     // 3rd bus details.
@@ -109,45 +105,45 @@ async function fetchBusData () {
     // Apply BUS Line status text and CSS as per the line ID.
     const routeStatus3 = document.getElementById('route-status-3')
     // If bus route status is not found, assign 22 as error.
-    const bus3_route_status = (lineData[bus3LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
+    const bus3RouteStatus = (lineData[bus3LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
 
-    routeStatus3.innerHTML = getStatusInfo(bus3_route_status).message
-    routeStatus3.className = getStatusInfo(bus3_route_status).className
+    routeStatus3.innerHTML = getStatusInfo(bus3RouteStatus).message
+    routeStatus3.className = getStatusInfo(bus3RouteStatus).className
 
     // check if the API have bus information, if not, assign zero to hide it.
-    const bus3_Line = nextBuses.length > 2 && nextBuses[2].lineName ? nextBuses[2].lineName : 0
-    const bus3_Destination = nextBuses.length > 2 && nextBuses[2].destinationName ? nextBuses[2].destinationName : 0;
-    const bus3_time = nextBuses.length > 2 && nextBuses[2].timeToStation ? Math.floor(nextBuses[2].timeToStation / 60) : 0
+    const bus3Line = nextBuses.length > 2 && nextBuses[2].lineName ? nextBuses[2].lineName : 0
+    const bus3Destination = nextBuses.length > 2 && nextBuses[2].destinationName ? nextBuses[2].destinationName : 0;
+    const bus3Time = nextBuses.length > 2 && nextBuses[2].timeToStation ? Math.floor(nextBuses[2].timeToStation / 60) : 0
 
-    if (bus3_Line === 0 && bus3_Destination === 0 && bus3_time === 0) {
+    if (bus3Line === 0 && bus3Destination === 0 && bus3Time === 0) {
       document.querySelector('.bus-item-3').classList.add('hidden')
     }
 
-    const bus3_routeElement = document.querySelector('.route-3').innerHTML = `Route ${bus3_Line}&nbsp;`
-    const bus3_destinationElement = document.querySelector('.destination-3').innerHTML = `(${bus3_Destination})`
-    const bus3_timeElement = document.querySelector('.time-3').innerHTML = `${bus3_time} MIN`
+    document.querySelector('.route-3').innerHTML = `Route ${bus3Line}&nbsp;`
+    document.querySelector('.destination-3').innerHTML = `(${bus3Destination})`
+    document.querySelector('.time-3').innerHTML = `${bus3Time} MIN`
 
     // 4th bus details.
 
     // Apply BUS Line status text and CSS as per the line ID.
     const routeStatus4 = document.getElementById('route-status-4')
     // If bus route status is not found, assign 22 as error.
-    const bus4_route_status = (lineData[bus4LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
+    const bus4RouteStatus = (lineData[bus4LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
 
-    routeStatus4.innerHTML = getStatusInfo(bus4_route_status).message
-    routeStatus4.className = getStatusInfo(bus4_route_status).className;
+    routeStatus4.innerHTML = getStatusInfo(bus4RouteStatus).message
+    routeStatus4.className = getStatusInfo(bus4RouteStatus).className;
 
-    const bus4_Line = nextBuses.length > 3 && nextBuses[3].lineName ? nextBuses[3].lineName : 0
-    const bus4_Destination = nextBuses.length > 3 && nextBuses[3].destinationName ? nextBuses[3].destinationName : 0
-    const bus4_time = nextBuses.length > 3 && nextBuses[3].timeToStation ? Math.floor(nextBuses[3].timeToStation / 60) : 0
+    const bus4Line = nextBuses.length > 3 && nextBuses[3].lineName ? nextBuses[3].lineName : 0
+    const bus4Destination = nextBuses.length > 3 && nextBuses[3].destinationName ? nextBuses[3].destinationName : 0
+    const bus4Time = nextBuses.length > 3 && nextBuses[3].timeToStation ? Math.floor(nextBuses[3].timeToStation / 60) : 0
 
-    if (bus4_Line === 0 && bus4_Destination === 0 && bus4_time === 0) {
+    if (bus4Line === 0 && bus4Destination === 0 && bus4Time === 0) {
       document.querySelector('.bus-item-4').classList.add('hidden')
     }
 
-    const bus4_routeElement = document.querySelector('.route-4').innerHTML = `Route ${bus4_Line}&nbsp;`
-    const bus4_destinationElement = document.querySelector('.destination-4').innerHTML = `(${bus4_Destination})`
-    const bus4_timeElement = document.querySelector('.time-4').innerHTML = `${bus4_time} MIN`
+    document.querySelector('.route-4').innerHTML = `Route ${bus4Line}&nbsp;`
+    document.querySelector('.destination-4').innerHTML = `(${bus4Destination})`
+    document.querySelector('.time-4').innerHTML = `${bus4Time} MIN`
 
 
     // 5th bus details.
@@ -155,22 +151,22 @@ async function fetchBusData () {
     // Apply BUS Line status text and CSS as per the line ID.
     const routeStatus5 = document.getElementById('route-status-5')
     // If bus route status is not found, assign 22 as error.
-    const bus5_route_status = (lineData[bus5LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
+    const bus5RouteStatus = (lineData[bus5LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
 
-    routeStatus5.innerHTML = getStatusInfo(bus5_route_status).message
-    routeStatus5.className = getStatusInfo(bus5_route_status).className
+    routeStatus5.innerHTML = getStatusInfo(bus5RouteStatus).message
+    routeStatus5.className = getStatusInfo(bus5RouteStatus).className
 
-    const bus5_Line = nextBuses.length > 4 && nextBuses[4].lineName ? nextBuses[4].lineName : 0;
-    const bus5_Destination = nextBuses.length > 4 && nextBuses[4].destinationName ? nextBuses[4].destinationName : 0
-    const bus5_time = nextBuses.length > 4 && nextBuses[4].timeToStation ? Math.floor(nextBuses[4].timeToStation / 60) : 0
+    const bus5Line = nextBuses.length > 4 && nextBuses[4].lineName ? nextBuses[4].lineName : 0;
+    const bus5Destination = nextBuses.length > 4 && nextBuses[4].destinationName ? nextBuses[4].destinationName : 0
+    const bus5Time = nextBuses.length > 4 && nextBuses[4].timeToStation ? Math.floor(nextBuses[4].timeToStation / 60) : 0
 
-    if (bus5_Line === 0 && bus5_Destination === 0 && bus5_time === 0) {
+    if (bus5Line === 0 && bus5Destination === 0 && bus5Time === 0) {
       document.querySelector('.bus-item-5').classList.add('hidden')
     }
 
-    const bus5_routeElement = document.querySelector('.route-5').innerHTML = `Route ${bus5_Line}&nbsp;`
-    const bus5_destinationElement = document.querySelector('.destination-5').innerHTML = `(${bus5_Destination})`
-    const bus5_timeElement = document.querySelector('.time-5').innerHTML = `${bus5_time} MIN`
+    document.querySelector('.route-5').innerHTML = `Route ${bus5Line}&nbsp;`
+    document.querySelector('.destination-5').innerHTML = `(${bus5Destination})`
+    document.querySelector('.time-5').innerHTML = `${bus5Time} MIN`
 
 
     // 6th bus details.
@@ -178,45 +174,45 @@ async function fetchBusData () {
     //Apply BUS Line status text and CSS as per the line ID.
     const routeStatus6 = document.getElementById('route-status-6')
     // If bus route status is not found, assign 22 as error.
-    const bus6_route_status = (lineData[bus6LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
+    const bus6RouteStatus = (lineData[bus6LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
 
-    routeStatus6.innerHTML = getStatusInfo(bus6_route_status).message
-    routeStatus6.className = getStatusInfo(bus6_route_status).className
+    routeStatus6.innerHTML = getStatusInfo(bus6RouteStatus).message
+    routeStatus6.className = getStatusInfo(bus6RouteStatus).className
 
     // check if the bus info are available in API.
-    const bus6_Line = nextBuses.length > 5 && nextBuses[5].lineName ? nextBuses[5].lineName : 0
-    const bus6_Destination = nextBuses.length > 5 && nextBuses[5].destinationName ? nextBuses[5].destinationName : 0
-    const bus6_time = nextBuses.length > 5 && nextBuses[5].timeToStation ? Math.floor(nextBuses[5].timeToStation / 60) : 0
+    const bus6Line = nextBuses.length > 5 && nextBuses[5].lineName ? nextBuses[5].lineName : 0
+    const bus6Destination = nextBuses.length > 5 && nextBuses[5].destinationName ? nextBuses[5].destinationName : 0
+    const bus6Time = nextBuses.length > 5 && nextBuses[5].timeToStation ? Math.floor(nextBuses[5].timeToStation / 60) : 0
 
-    if (bus6_Line === 0 && bus6_Destination === 0 && bus6_time === 0) {
+    if (bus6Line === 0 && bus6Destination === 0 && bus6Time === 0) {
       document.querySelector('.bus-item-6').classList.add('hidden')
     }
 
-    document.querySelector('.route-6').innerHTML = `Route ${bus6_Line}&nbsp;`
-    document.querySelector('.destination-6').innerHTML = `(${bus6_Destination})`
-    document.querySelector('.time-6').innerHTML = `${bus6_time} MIN`
+    document.querySelector('.route-6').innerHTML = `Route ${bus6Line}&nbsp;`
+    document.querySelector('.destination-6').innerHTML = `(${bus6Destination})`
+    document.querySelector('.time-6').innerHTML = `${bus6Time} MIN`
 
     // 7th bus details
 
     // Apply BUS Line status text and CSS as per the line ID.
     const routeStatus7 = document.getElementById('route-status-7')
     // If bus route status is not found, assign 22 as error.
-    const bus7_route_status = (lineData[bus7LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
+    const bus7RouteStatus = (lineData[bus7LineID]?.lineStatuses?.[0]?.statusSeverity) ?? 22
 
-    routeStatus7.innerHTML = getStatusInfo(bus7_route_status).message
-    routeStatus7.className = getStatusInfo(bus7_route_status).className
+    routeStatus7.innerHTML = getStatusInfo(bus7RouteStatus).message
+    routeStatus7.className = getStatusInfo(bus7RouteStatus).className
 
-    const bus7_Line = nextBuses.length > 6 && nextBuses[6].lineName ? nextBuses[6].lineName : 0
-    const bus7_Destination = nextBuses.length > 6 && nextBuses[6].destinationName ? nextBuses[6].destinationName : 0
-    const bus7_time = nextBuses.length > 6 && nextBuses[6].timeToStation ? Math.floor(nextBuses[6].timeToStation / 60) : 0
+    const bus7Line = nextBuses.length > 6 && nextBuses[6].lineName ? nextBuses[6].lineName : 0
+    const bus7Destination = nextBuses.length > 6 && nextBuses[6].destinationName ? nextBuses[6].destinationName : 0
+    const bus7Time = nextBuses.length > 6 && nextBuses[6].timeToStation ? Math.floor(nextBuses[6].timeToStation / 60) : 0
 
-    if (bus7_Line === 0 && bus7_Destination === 0 && bus7_time === 0) {
+    if (bus7Line === 0 && bus7Destination === 0 && bus7Time === 0) {
       document.querySelector('.bus-item-7').classList.add('hidden')
     }
 
-    document.querySelector('.route-7').innerHTML = `Route ${bus7_Line}&nbsp;`
-    document.querySelector('.destination-7').innerHTML = `(${bus7_Destination})`
-    document.querySelector('.time-7').innerHTML = `${bus7_time} MIN`
+    document.querySelector('.route-7').innerHTML = `Route ${bus7Line}&nbsp;`
+    document.querySelector('.destination-7').innerHTML = `(${bus7Destination})`
+    document.querySelector('.time-7').innerHTML = `${bus7Time} MIN`
 
   } catch (error) {
     console.error('Error fetching bus data:', error);
