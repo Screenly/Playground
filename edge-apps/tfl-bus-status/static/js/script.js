d1d7c40d867328541e3c3409c50f9d923e88aeaf
Salman Faris <farissalmannbr@gmail.com>
2024-09-10 20:49:27 +0530

    fix git conflict with reset to head one

--- edge-apps/tfl-bus-status/static/js/script.js
+++ edge-apps/tfl-bus-status/static/js/script.js
@@ -3,7 +3,6 @@
 const apiUrl = 'https://api.tfl.gov.uk/' // Base URL for the TfL API
 const stopId = screenly.settings.stop_id
 const apiKey = screenly.settings.tfl_api_token
-const sentryDsn = screenly.settings.sentry_dsn
 
 async function getCachedData (url, cacheKey) {
   const cachedData = JSON.parse(localStorage.getItem(cacheKey))
