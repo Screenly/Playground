8aa163624c020be9330fe2823640d7f7f23ac570
nicomiguelino <nicomiguelino2014@gmail.com>
2025-02-26 18:27:41 -0800

    chore: split JSX components into multiple files

---
+++ edge-apps/google-calendar/src/components/calendar-overview.jsx
@@ -0,0 +1,24 @@
+const CalendarOverview = ({ currentDate, currentMonthName, currentTime, events }) => {
+  return (
+    <div className="secondary-card calendar-overview-card">
+      <div className="calendar-top">
+        <h1 className="calendar-date">
+          <span className="date-number-box">{currentDate}</span>
+          {currentMonthName}
+        </h1>
+        <div className="calendar-event">
+          {events.length > 0 ? (
+            events.map((event, index) => <p key={index}>{event}</p>)
+          ) : (
+            <p>Nothing scheduled on this date</p>
+          )}
+        </div>
+      </div>
+      <div className="calendar-bottom">
+        <h1 className="calendar-time">{currentTime}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default CalendarOverview;
