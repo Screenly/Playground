d9cc971fc7c23efbaaf555b409a88e685cf39d3b
nicomiguelino <nicomiguelino2014@gmail.com>
2025-03-09 19:00:55 -0700

    chore: install ESLint for linting code

---
+++ edge-apps/google-calendar/eslint.config.mjs
@@ -0,0 +1,18 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+
+export default [
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error"
+    },
+    languageOptions: {
+      globals: {
+        screenly: "readonly",
+        ...globals.browser,
+      },
+    },
+  }
+];
