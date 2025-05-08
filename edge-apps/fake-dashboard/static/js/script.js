3b9567fb9211d56c6c7d2fb8105001333086014e
Salman Faris <farissalmannbr@gmail.com>
2025-05-08 17:34:33 +0530

    added QC APP ID & App Logo

--- edge-apps/fake-dashboard/static/js/script.js
+++ edge-apps/fake-dashboard/static/js/script.js
@@ -1,5 +1,5 @@
 // Utility functions for generating random data
-/* global Chart */
+/* global Chart, screenly */
 
 function randomInt (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
