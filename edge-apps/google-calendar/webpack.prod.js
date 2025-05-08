478226bb74f97ba6d890516951a3ee21bac228a2
nicomiguelino <nicomiguelino2014@gmail.com>
2025-05-08 13:40:34 -0700

    fix: don't copy `instance.yml` in production

--- edge-apps/google-calendar/webpack.prod.js
+++ edge-apps/google-calendar/webpack.prod.js
@@ -1,17 +1,6 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'instance.yml',
-          to: 'instance.yml'
-        }
-      ]
-    })
-  ]
 })
