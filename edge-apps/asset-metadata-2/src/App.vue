d081a6721beefe58c5de6eaeae28370eb91e212a
nicomiguelino <nicomiguelino2014@gmail.com>
2025-07-17 10:40:04 -0700

    fix: copy files from the original asset metadata app

--- edge-apps/asset-metadata-2/src/App.vue
+++ edge-apps/asset-metadata-2/src/App.vue
@@ -1,17 +1,8 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { useRootStore } from './stores/root-store'
+import { useScreenlyMetadataStore } from './stores/root-store'
 
-const rootStore = useRootStore()
-const {
-  coordinates,
-  hostname,
-  hardware,
-  location,
-  screenly_version: screenlyVersion,
-  screen_name: screenName,
-  tags,
-} = screenly.metadata
+const screenlyMetadataStore = useScreenlyMetadataStore()
 
 onMounted(() => {
   screenly.signalReadyForRendering()
@@ -19,31 +10,104 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="container">
-    <h1
-      :style="{
-        marginBottom: '1rem',
-      }"
-    >
-      {{ rootStore.message }}
-    </h1>
-    <p><strong>Coordinates:</strong> {{ coordinates.join(', ') }}</p>
-    <p><strong>Hostname:</strong> {{ hostname }}</p>
-    <p><strong>Hardware:</strong> {{ hardware }}</p>
-    <p><strong>Location:</strong> {{ location }}</p>
-    <p><strong>Screenly Version:</strong> {{ screenlyVersion }}</p>
-    <p><strong>Screen Name:</strong> {{ screenName }}</p>
-    <p><strong>Tags:</strong> {{ tags.join(', ') }}</p>
+  <div class="main-container main-container-grid">
+    <div class="primary-card host-name-card">
+      <div class="icon-card">
+        <object
+          id="name-icon"
+          type="image/svg+xml"
+          data="static/images/icons/name-icon.svg"
+          class="icon-card-icon"
+          alt="icon"
+        ></object>
+        <span class="icon-card-text head-text">Host Name</span>
+      </div>
+      <span class="icon-card-text host-name-text">
+        {{ screenlyMetadataStore.hostname }}
+      </span>
+    </div>
+    <div class="primary-card screen-name-card">
+      <div class="icon-card">
+        <object
+          id="name-icon"
+          type="image/svg+xml"
+          data="./static/images/icons/name-icon.svg"
+          class="icon-card-icon"
+          alt="icon"
+        ></object>
+        <span class="icon-card-text head-text">Name</span>
+      </div>
+      <span class="icon-card-text screen-name-text">
+        {{ screenlyMetadataStore.screenName }}
+      </span>
+    </div>
+    <div class="secondary-card hardware-name-card">
+      <div class="icon-card">
+        <object
+          id="hardware-icon"
+          type="image/svg+xml"
+          data="./static/images/icons/hardware-icon.svg"
+          class="icon-card-icon"
+          alt="icon"
+        ></object>
+        <span class="icon-card-text head-text">Hardware</span>
+      </div>
+      <span class="icon-card-text hardware-name-text">
+        {{ screenlyMetadataStore.hardware }}
+      </span>
+    </div>
+    <div class="secondary-card brand-logo-card">
+      <img id="brand-logo" src="./assets/images/screenly.svg" class="brand-logo" alt="Brand Logo" />
+      <span class="info-text">Powered by Screenly</span>
+    </div>
+    <div class="secondary-card version-name-card">
+      <div class="icon-card">
+        <object
+          id="version-icon"
+          type="image/svg+xml"
+          data="./static/images/icons/version-icon.svg"
+          class="icon-card-icon"
+          alt="icon"
+        ></object>
+        <span class="icon-card-text head-text">Version</span>
+      </div>
+      <span class="icon-card-text version-name-text">
+        {{ screenlyMetadataStore.screenlyVersion }}
+      </span>
+    </div>
+    <div class="secondary-card coordinates-card">
+      <div class="icon-card">
+        <object
+          id="coordinates-icon"
+          type="image/svg+xml"
+          data="./static/images/icons/coordinates-icon.svg"
+          class="icon-card-icon"
+          alt="icon"
+        ></object>
+        <span class="icon-card-text head-text">Coordinates</span>
+      </div>
+      <span class="icon-card-text coordinates-name-text">
+        {{ screenlyMetadataStore.formattedCoordinates }}
+      </span>
+    </div>
+    <div class="secondary-card labels-name-card">
+      <div class="icon-card">
+        <object
+          id="version-icon"
+          type="image/svg+xml"
+          data="./static/images/icons/version-icon.svg"
+          class="icon-card-icon"
+          alt="icon"
+        ></object>
+        <span class="icon-card-text head-text">Labels</span>
+      </div>
+      <div class="label-chip-container">
+        <div class="label-chip" v-for="tag in screenlyMetadataStore.tags" :key="tag">
+          {{ tag }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-.container {
-  display: flex;
-  gap: 0.75rem;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-}
-</style>
+<style scoped></style>
--- edge-apps/asset-metadata-2/src/stores/root-store.ts
+++ edge-apps/asset-metadata-2/src/stores/root-store.ts
@@ -1,8 +1,36 @@
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useRootStore = defineStore('root', () => {
   const message = ref('Hello!')
 
   return { message }
+})
+
+export const useScreenlyMetadataStore = defineStore('metadata', () => {
+  const metadata = screenly.metadata
+
+  const coordinates = ref(metadata.coordinates ?? '')
+  const hostname = ref(metadata.hostname ?? '')
+  const screenName = ref(metadata.screen_name ?? '')
+  const hardware = ref(metadata.hardware ?? '')
+  const location = ref(metadata.location ?? '')
+  const screenlyVersion = ref(metadata.screenly_version ?? '')
+  const tags = ref(metadata.tags ?? [])
+
+  const formattedCoordinates = computed(() => {
+    const [latitude, longitude] = coordinates.value
+    return `${latitude}, ${longitude}`
+  })
+
+  return {
+    coordinates,
+    hostname,
+    screenName,
+    hardware,
+    location,
+    screenlyVersion,
+    tags,
+    formattedCoordinates,
+  }
 })
