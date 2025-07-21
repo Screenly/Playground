f9e2a4e3dd14cdae7b6076d6d07e2d681fd672f4
nicomiguelino <nicomiguelino2014@gmail.com>
2025-07-21 10:57:32 -0700

    chore(ci): fix JS linting errors in `stylelint.config.js`

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   overrides: [
     {
-      files: ["**/*.scss"],
-      customSyntax: "postcss-scss",
-    },
+      files: ['**/*.scss'],
+      customSyntax: 'postcss-scss'
+    }
   ],
   rules: {
     // Add your custom Stylelint rules here
-  },
-};
+  }
+}
