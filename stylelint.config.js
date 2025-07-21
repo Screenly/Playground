f3eddef40612421e3ae56c80c2ec23a26c0c1596
nicomiguelino <nicomiguelino2014@gmail.com>
2025-07-21 10:54:00 -0700

    chore(ci): create `stylelint.config.js`

---
+++ stylelint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  overrides: [
+    {
+      files: ["**/*.scss"],
+      customSyntax: "postcss-scss",
+    },
+  ],
+  rules: {
+    // Add your custom Stylelint rules here
+  },
+};
