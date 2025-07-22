01df566c9df069001bf1a88280c249ca4e1c4672
nicomiguelino <nicomiguelino2014@gmail.com>
2025-07-22 11:54:32 -0700

    chore: import base SCSS and custom Vite plugin via dependency instead

--- edge-apps/.bun-create/edge-app-template/vite.config.ts
+++ edge-apps/.bun-create/edge-app-template/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import { existsSync } from 'fs'
-import { screenlyPlugin } from './screenly-vite-plugin'
+import { screenlyPlugin } from 'screenly-playground/vite'
 
 const manifestFileName = process.env.MANIFEST_FILE_NAME || 'screenly.yml'
 
