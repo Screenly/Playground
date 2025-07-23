fc458ae04d7d329205e4a149eebfb913b0270c03
nicomiguelino <nicomiguelino2014@gmail.com>
2025-07-23 15:51:32 -0700

    chore: replace occurrences of `npm run` with `bun run`

--- edge-apps/.bun-create/edge-app-template/playwright.config.ts
+++ edge-apps/.bun-create/edge-app-template/playwright.config.ts
@@ -103,7 +103,7 @@ export default defineConfig({
      * Use the preview server on CI for more realistic testing.
      * Playwright will re-use the local server if there is already a dev-server running.
      */
-    command: process.env.CI ? 'npm run preview' : 'npm run dev',
+    command: process.env.CI ? 'bun run preview' : 'bun run dev',
     port: process.env.CI ? 4173 : 5173,
     reuseExistingServer: !process.env.CI,
   },
