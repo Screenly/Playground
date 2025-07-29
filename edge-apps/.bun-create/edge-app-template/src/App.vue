eb26efe6154c56efaa30e63036db4ed745b902da
nicomiguelino <nicomiguelino2014@gmail.com>
2025-07-29 14:00:57 -0700

    feat: create a reusable component for the primary card

--- edge-apps/.bun-create/edge-app-template/src/App.vue
+++ edge-apps/.bun-create/edge-app-template/src/App.vue
@@ -3,6 +3,7 @@ import { onBeforeMount, onMounted, ref, type Ref } from 'vue'
 import { defineStore, storeToRefs } from 'pinia'
 import { metadataStoreSetup } from 'blueprint/stores/metadata-store'
 import { settingsStoreSetup } from 'blueprint/stores/settings-store'
+import { PrimaryCard } from 'blueprint/components'
 
 const useScreenlyMetadataStore = defineStore('metadata', metadataStoreSetup)
 const useSettingsStore = defineStore('settingsStore', settingsStoreSetup)
@@ -35,48 +36,44 @@ onMounted(() => {
 
 <template>
   <div class="main-container main-container-grid">
-    <div class="primary-container">
-      <div class="primary-card">
-        <div class="px-4 py-5 my-5 text-center">
-          <h1 class="main-header display-5 fw-bold">
-            <span id="greeting">
-              <template v-if="greeting"> Greetings, {{ greeting }}! </template>
-              <template v-else> Greetings! </template>
-            </span>
-          </h1>
+    <PrimaryCard>
+      <h1 class="main-header">
+        <span id="greeting">
+          <template v-if="greeting"> Greetings, {{ greeting }}! </template>
+          <template v-else> Greetings! </template>
+        </span>
+      </h1>
 
-          <p>
-            You secret word is
-            <template v-if="secretWord">
-              <strong>{{ secretWord }}</strong
-              >.
-            </template>
-            <template v-else> not set. </template>
-          </p>
+      <p>
+        You secret word is
+        <template v-if="secretWord">
+          <strong>{{ secretWord }}</strong
+          >.
+        </template>
+        <template v-else> not set. </template>
+      </p>
 
-          <p>
-            I'm <span id="screen-name">{{ screenName }}</span
-            >. Assuming you've pinned me in the right location,<br />I'm located
-            in <span id="screen-location">{{ location }}</span> (more precisely
-            at latitude <span id="screen-lat">{{ coordinates[0] }}</span
-            >&#176; and longitude
-            <span id="screen-lng">{{ coordinates[1] }}</span
-            >&#176;).
-          </p>
+      <p>
+        I'm <strong id="screen-name">{{ screenName }}</strong
+        >. Assuming you've pinned me in the right location,<br />I'm located in
+        <strong id="screen-location">{{ location }}</strong> (more precisely at
+        latitude <strong id="screen-lat">{{ coordinates[0] }}</strong
+        >&#176; and longitude
+        <strong id="screen-lng">{{ coordinates[1] }}</strong
+        >&#176;).
+      </p>
 
-          <p>
-            My Screenly ID is
-            <span id="screen-hostname">
-              <strong>{{ hostname }}</strong>
-            </span>
-            (which conveniently is also my hostname), and I'm running on a
-            <span id="screen-hardware">
-              <strong>{{ hardware || 'virtual screen' }}</strong> </span
-            >.
-          </p>
-        </div>
-      </div>
-    </div>
+      <p>
+        My Screenly ID is
+        <span id="screen-hostname">
+          <strong>{{ hostname }}</strong>
+        </span>
+        (which conveniently is also my hostname), and I'm running on a
+        <span id="screen-hardware">
+          <strong>{{ hardware || 'virtual screen' }}</strong> </span
+        >.
+      </p>
+    </PrimaryCard>
   </div>
 </template>
 
@@ -95,41 +92,23 @@ onMounted(() => {
   }
 }
 
-@mixin primary-card-padding($multiplier: 1) {
-  padding: 5rem * $multiplier;
+@media (min-width: 800px) and (min-height: 480px) and (orientation: landscape) {
+  @include header-text(1);
+  @include content-text(1);
 }
 
-.primary-container {
-  height: 100%;
-  width: 100%;
+@media (min-width: 1280px) and (min-height: 720px) and (orientation: landscape) {
+  @include header-text(1.5);
+  @include content-text(1.5);
 }
 
-.primary-card {
-  height: 100%;
-  width: 100%;
+@media (min-width: 1920px) and (min-height: 1080px) and (orientation: landscape) {
+  @include header-text(2.25);
+  @include content-text(2.25);
+}
 
-  @media (min-width: 800px) and (min-height: 480px) and (orientation: landscape) {
-    @include header-text(1);
-    @include content-text(1);
-    @include primary-card-padding(1);
-  }
-
-  @media (min-width: 1280px) and (min-height: 720px) and (orientation: landscape) {
-    @include header-text(1.5);
-    @include content-text(1.5);
-    @include primary-card-padding(1.5);
-  }
-
-  @media (min-width: 1920px) and (min-height: 1080px) and (orientation: landscape) {
-    @include header-text(2.25);
-    @include content-text(2.25);
-    @include primary-card-padding(2.25);
-  }
-
-  @media (min-width: 3840px) and (orientation: landscape) {
-    @include header-text(4.5);
-    @include content-text(4.5);
-    @include primary-card-padding(4.5);
-  }
+@media (min-width: 3840px) and (orientation: landscape) {
+  @include header-text(4.5);
+  @include content-text(4.5);
 }
 </style>
--- edge-apps/blueprint/ts/components/index.ts
+++ edge-apps/blueprint/ts/components/index.ts
@@ -1,2 +1,3 @@
 export { default as InfoCard } from './InfoCard.vue'
+export { default as PrimaryCard } from './PrimaryCard.vue'
 export * from './icons'
