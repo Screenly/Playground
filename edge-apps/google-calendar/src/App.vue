f7f0942ecfc406281530aa954ba3f72bdaf9aba7
nicomiguelino <nicomiguelino2014@gmail.com>
2025-08-06 21:58:05 -0700

    feat: make use of the analog clock from the shared library

--- edge-apps/google-calendar/src/App.vue
+++ edge-apps/google-calendar/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { computed, onMounted } from 'vue'
+
+import { AnalogClock } from 'blueprint/components'
+
 import { useCalendarStore } from '@/stores/calendar'
 import MonthlyCalendarView from '@/components/MonthlyCalendarView.vue'
 import CalendarOverview from '@/components/CalendarOverview.vue'
 import InfoCard from '@/components/InfoCard.vue'
 import DailyCalendarView from '@/components/DailyCalendarView.vue'
-import AnalogClock from '@/components/AnalogClock.vue'
 import WeeklyCalendarView from '@/components/WeeklyCalendarView.vue'
 
 const calendarStore = useCalendarStore()
@@ -29,12 +31,53 @@ onMounted(async () => {
       </div>
       <div class="row-container">
         <CalendarOverview v-if="calendarMode === 'monthly'" />
-        <AnalogClock v-else />
+        <div
+          class="secondary-card"
+          :style="{
+            backgroundColor: 'var(--theme-color-primary)',
+          }"
+        >
+          <AnalogClock />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-/* Component-specific styles if needed */
+<style scoped lang="scss">
+$base-scale: 0.25;
+
+.clock-container {
+  @media screen and (min-width: 800px) and (orientation: landscape) {
+    transform: scale($base-scale);
+  }
+
+  @media screen and (min-width: 1280px) and (orientation: landscape) {
+    transform: scale($base-scale * 1.5);
+  }
+
+  @media screen and (min-width: 1920px) and (orientation: landscape) {
+    transform: scale($base-scale * 2.25);
+  }
+
+  @media screen and (min-width: 3840px) and (orientation: landscape) {
+    transform: scale($base-scale * 4.5);
+  }
+
+  @media screen and (min-width: 480px) and (orientation: portrait) {
+    transform: scale($base-scale * 1);
+  }
+
+  @media screen and (min-width: 720px) and (orientation: portrait) {
+    transform: scale($base-scale * 1.6);
+  }
+
+  @media screen and (min-width: 1080px) and (orientation: portrait) {
+    transform: scale($base-scale * 2.4);
+  }
+
+  @media screen and (min-width: 2160px) and (orientation: portrait) {
+    transform: scale($base-scale * 4.8);
+  }
+}
 </style>
