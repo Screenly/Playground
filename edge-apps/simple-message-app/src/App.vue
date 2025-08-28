f955c7cde541f2190ecd0c97a7ebaa1d83e09603
nicomiguelino <nicomiguelino2014@gmail.com>
2025-08-27 18:34:39 -0700

    chore: fix linting and formatting issues

--- edge-apps/simple-message-app/src/App.vue
+++ edge-apps/simple-message-app/src/App.vue
@@ -1,31 +1,15 @@
 <script setup lang="ts">
-import { onBeforeMount, onMounted, ref, type Ref } from 'vue'
-import { defineStore, storeToRefs } from 'pinia'
-import { metadataStoreSetup } from 'blueprint/stores/metadata-store'
+import { onBeforeMount, onMounted } from 'vue'
+import { defineStore } from 'pinia'
 import { baseSettingsStoreSetup } from 'blueprint/stores/base-settings-store'
-import { PrimaryCard, AnalogClock } from 'blueprint/components'
+import { AnalogClock } from 'blueprint/components'
 
-const useScreenlyMetadataStore = defineStore('metadata', metadataStoreSetup)
 const useBaseSettingsStore = defineStore(
   'baseSettingsStore',
   baseSettingsStoreSetup,
 )
 
-const screenlyMetadataStore = useScreenlyMetadataStore()
 const baseSettingsStore = useBaseSettingsStore()
-
-const { hostname, screenName, hardware, coordinates, location } = storeToRefs(
-  screenlyMetadataStore,
-) as unknown as {
-  hostname: Ref<string>
-  screenName: Ref<string>
-  hardware: Ref<string>
-  coordinates: Ref<[number, number]>
-  location: Ref<string>
-}
-
-const secretWord = ref(screenly.settings.secret_word)
-const greeting = ref(screenly.settings.greeting)
 
 onBeforeMount(async () => {
   baseSettingsStore.setupTheme()
