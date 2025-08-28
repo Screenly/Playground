116e05dfe790e7d39ca7b57916039e768637f800
nicomiguelino <nicomiguelino2014@gmail.com>
2025-08-28 12:01:49 -0700

    chore: create a new component for the message body

--- edge-apps/simple-message-app/src/App.vue
+++ edge-apps/simple-message-app/src/App.vue
@@ -6,6 +6,7 @@ import { baseSettingsStoreSetup } from 'blueprint/stores/base-settings-store'
 import { AnalogClock, BrandLogoCard, DateDisplay } from 'blueprint/components'
 import { useSettingsStore } from '@/stores/settings'
 import MessageHead from '@/components/MessageHead.vue'
+import MessageBody from '@/components/MessageBody.vue'
 
 import screenlyLogo from 'blueprint/assets/images/screenly.svg'
 
@@ -50,18 +51,15 @@ onMounted(() => {
     </div>
 
     <div class="secondary-container">
-      <div class="message-body secondary-card">
-        <span class="message-body-content"
-          >A simple message app allows users to display text on a screen, making
-          it a basic tool for digital signage. Users can input and edit both the
-          heading and message body directly from the Screenly dashboard.</span
-        >
-      </div>
+      <MessageBody />
     </div>
     <!-- Row Container with modules -->
     <div class="row-container">
       <div class="secondary-card">
-        <BrandLogoCard :logo-src="brandLogoUrl || screenlyLogo" />
+        <BrandLogoCard
+          :logo-src="brandLogoUrl || screenlyLogo"
+          class="info-card"
+        />
       </div>
 
       <div class="secondary-card">
@@ -86,20 +84,17 @@ onMounted(() => {
 // TODO: Move to a separate SCSS file.
 
 .main-container-grid {
-  display: grid !important;
-  grid-template-rows: 2fr 0.5fr 0.125fr 0.25fr 0.125fr 0.25fr 0.5fr !important;
-  grid-template-columns: 3fr 0.25fr 0.5fr 0.5fr 0.5fr !important;
-  gap: 2rem !important;
-  height: 100% !important;
-  width: 100% !important;
+  display: grid;
+  grid-template-rows: 2fr 0.5fr 0.125fr 0.25fr 0.125fr 0.25fr 0.5fr;
+  grid-template-columns: 3fr 0.25fr 0.5fr 0.5fr 0.5fr;
 
   .primary-container {
-    width: 100% !important;
-    grid-area: 1 / 1 / 4 / 3 !important;
+    width: 100%;
+    grid-area: 1 / 1 / 4 / 3;
 
     .primary-card {
-      width: 100% !important;
-      height: 100% !important;
+      width: 100%;
+      height: 100%;
     }
   }
 
@@ -123,136 +118,11 @@ onMounted(() => {
       flex: 1 1 33.333% !important;
       max-width: 33.333% !important;
       min-width: 0 !important;
+
+      .info-card {
+        gap: 2rem;
+      }
     }
   }
-}
-
-.message-body-content {
-  font-size: 6rem;
-  padding: 10rem;
-  line-height: 1.25;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  text-overflow: ellipsis;
-  -webkit-line-clamp: 17;
-  height: calc(1.14em * 20);
-  overflow: hidden;
-
-  // Responsive font sizing to match legacy app
-  @media screen and (max-width: 3840px) and (orientation: landscape) {
-    padding: 9rem;
-  }
-
-  @media screen and (max-width: 2560px) and (orientation: landscape) {
-    font-size: 3.2rem;
-    padding: 4rem;
-  }
-
-  @media screen and (max-width: 1920px) and (orientation: landscape) {
-    font-size: 3rem;
-    padding: 4rem;
-    -webkit-line-clamp: 17;
-    height: calc(1.13em * 20);
-  }
-
-  @media screen and (max-width: 1680px) and (orientation: landscape) {
-    font-size: 2.5rem;
-    padding: 3rem;
-    -webkit-line-clamp: 18;
-    height: calc(1.13em * 21);
-  }
-
-  @media screen and (max-width: 1600px) and (orientation: landscape) {
-    font-size: 2.3rem;
-  }
-
-  @media screen and (max-width: 1366px) and (orientation: landscape) {
-    font-size: 1.95rem;
-    height: calc(1.15em * 21);
-  }
-
-  @media screen and (max-width: 1280px) and (orientation: landscape) {
-    font-size: 2rem;
-    padding: 2.5rem;
-    -webkit-line-clamp: 17;
-    height: calc(1.13em * 20);
-  }
-
-  @media screen and (max-width: 1080px) and (orientation: landscape) {
-    font-size: 2rem;
-    padding: 2.5rem;
-  }
-
-  @media screen and (max-width: 1024px) and (orientation: landscape) {
-    font-size: 1.5rem;
-    height: calc(1.16em * 20);
-  }
-
-  @media screen and (max-width: 800px) and (orientation: landscape) {
-    font-size: 1.25rem;
-    padding: 2.5rem;
-    -webkit-line-clamp: 18;
-    height: calc(1.12em * 22);
-  }
-
-  // Portrait responsive sizes
-  @media screen and (max-width: 2160px) and (orientation: portrait) {
-    padding: 6rem;
-    -webkit-line-clamp: 9;
-    height: calc(1.13em * 11);
-  }
-
-  @media screen and (max-width: 1080px) and (orientation: portrait) {
-    font-size: 3rem;
-    padding: 4rem;
-    -webkit-line-clamp: 8;
-    height: calc(1.13em * 10);
-  }
-
-  @media screen and (max-width: 720px) and (orientation: portrait) {
-    font-size: 2rem;
-    padding: 2.5rem;
-    -webkit-line-clamp: 8;
-    height: calc(1.14em * 10);
-  }
-
-  @media screen and (max-width: 480px) and (orientation: portrait) {
-    font-size: 1.3rem;
-    padding: 1.2rem;
-    -webkit-line-clamp: 8;
-    height: calc(1.23em * 9);
-  }
-}
-
-.message-body {
-  display: flex;
-  align-items: flex-start;
-  justify-content: normal;
-}
-
-/* Card Styles */
-.secondary-card {
-  display: flex !important;
-  flex-direction: column !important;
-  justify-content: center !important;
-  align-items: center !important;
-  width: 100% !important;
-  height: auto !important;
-  gap: 2rem !important;
-  border-radius: 3.481rem !important;
-  background-color: var(--theme-color-tertiary) !important;
-}
-
-.info-card {
-  gap: 2rem;
-}
-
-.brand-logo {
-  width: 12rem;
-}
-
-.info-text {
-  font-size: 1.75rem;
-  color: var(--theme-color-primary);
 }
 </style>
